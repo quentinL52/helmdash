43a7bac590cd9280a437d068620a1733b8c3f41b
Quentin Loumeau <loumeau.quentin@gmail.com>
2026-02-13 20:55:06 +0000

    le whiteboard ne s'etend toujours pas sur toute la zone il faut etendre

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -12,7 +12,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
         <Sidebar>
           <SidebarNav />
         </Sidebar>
-        <main className="flex-1 flex flex-col overflow-hidden">
+        <main className="flex-1 flex flex-col overflow-hidden min-w-0">
           <div className="p-4 sm:p-6 lg:p-8 flex-1 flex flex-col has-[.whiteboard-page]:p-0">
             <div className="md:hidden mb-4">
               <SidebarTrigger />
